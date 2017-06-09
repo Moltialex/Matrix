e9a310546b1bfed72c6835a58448e735102d0ca6
Alexis <amolter@datasystems.fr>
2017-06-09 15:45:26 +0200

    Add print and addition functions

--- Matrix_classical/matrix.cpp
+++ Matrix_classical/matrix.cpp
@@ -2,24 +2,44 @@
 
 using namespace std;
 
+template<size_t I, size_t J>
+void mx_print(double (&tab)[I][J]){
+		
+		for(int i= 0; i< I; i++){
+			for(int j= 0; j< J; j++){
+				cout << tab[i][j] << " ";
+			}
+			cout << endl;
+		}
+}
+
+template<size_t I, size_t J>
+void mx_addition(double (&mx1)[I][J], double (&mx2)[I][J]){
+
+	double mx[I][J];
+
+	for(int i= 0; i< I; i++){
+		for(int j= 0; j<J; j++){
+			mx[i][j] = mx1[i][j] + mx2[i][j];
+		}
+	}
+
+	mx_print(mx);
+}
+
 int main(){
 	
-	int const tabsizeX= 5;
-	int const tabsizeY= 5;
-	char tab[tabsizeX][tabsizeY];
+	int const tabsize= 5;
+	double tab[tabsize][tabsize];
 
-	for(int i= 0; i<tabsizeX; i++){
-		for(int j= 0; j<tabsizeY; j++){
-			tab[i][j]= '.';
+	for(int i= 0; i< tabsize; i++){
+		for(int j= 0; j< tabsize; j++){
+			tab[i][j]= 1.0;
 		}
 	}
 	
-	for(int i= 0; i<tabsizeX; i++){
-		for(int j= 0; j<tabsizeY; j++){
-			cout << tab[i][j] << " ";
-		}
-		cout << endl;
-	}
+	mx_print(tab);
+	mx_addition(tab,tab);
 
 	return 0;
 }
